c22bd89443e0df2a24d1c76836ac178f439b79f0
Thomas Musselman <tmusselman@gmail.com>
2022-08-27 14:51:59 -0600

    test: breweries index only shows in production beers

--- spec/features/beers/index_spec.rb
+++ spec/features/beers/index_spec.rb
@@ -6,16 +6,16 @@ RSpec.describe 'beers#index', type: :feature do
     @odell = Brewery.create(name: "Odell Brewing Company", city:"Fort Collins", state:"CO", ba_member: true, annual_production: 225000, founded: 1989)
     @oberon = @bells.beers.create(name: "Oberon", abv: 5.8, ibu: 10, style:"Wheat", in_production: true)
     @hopslam = @bells.beers.create(name: "Hopslam", abv:10.0 , ibu:65 , style:"Double IPA", in_production: true)
-    @ninety_shilling = @odell.beers.create(name: "90 Shilling", abv: 5.3, ibu: 27, style:"Red Ale", in_production: false)
+    @ninety_shilling = @odell.beers.create(name: "90 Shilling", abv: 5.3, ibu: 27, style:"Red Ale", in_production: true)
     @sippin_pretty = @odell.beers.create(name: "Sippin Pretty", abv: 4.5, ibu: 0, style:"Fruited Sour", in_production: false)
   end
 
-  it 'lists each child record found in the system' do
+  it 'lists each child record found in the system with boolean true' do
     visit('/beers')
 
     expect(page).to have_content(@oberon.name)
     expect(page).to have_content(@hopslam.name)
     expect(page).to have_content(@ninety_shilling.name)
-    expect(page).to have_content(@sippin_pretty.name)
+    expect(page).to_not have_content(@sippin_pretty.name)
   end
 end
