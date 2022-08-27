fe7cadb2c8b3f9548bdd0cb6aa2f2231c3dd61de
Thomas Musselman <tmusselman@gmail.com>
2022-08-27 15:26:54 -0600

    feature: sort brewery's beers by name

--- app/controllers/brewery_beers_controller.rb
+++ app/controllers/brewery_beers_controller.rb
@@ -1,7 +1,7 @@
 class BreweryBeersController < ApplicationController
   def index
     @brewery = Brewery.find(params[:brewery_id])
-    @beers = @brewery.beers
+    @beers = @brewery.beers.order(params[:sort_by])
   end
 
   def new
