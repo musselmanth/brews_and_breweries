7a1b660488b28543c5b002ebcd88983dc623e255
Thomas Musselman <tmusselman@gmail.com>
2022-08-27 13:14:01 -0600

    feature: add welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'welcome#index'
+
   get '/breweries', to: 'breweries#index'
   get '/breweries/new', to: 'breweries#new'
   get '/breweries/:brewery_id', to: 'breweries#show'
